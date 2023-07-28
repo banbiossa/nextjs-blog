22f6f70412d09a9a171e43436a1354d6d3f5a69d
shota shimizu <shota.shimizu@caddi.jp>
2023-07-28 16:21:23 +0900

    remove redundant children

--- components/layout.js
+++ components/layout.js
@@ -26,7 +26,6 @@ export default function Layout({ children, home }) {
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content='summary_large_image' />
       </Head>
-      {children}
       <header className={styles.header}>
         {home ? (
           <>
